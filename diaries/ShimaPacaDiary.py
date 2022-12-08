18a2246889e0ef56aa5e4a89ecd7c68e8f70cfa7
shima004 <yellow030408@gmail.com>
2022-12-08 15:48:06 +0900

    日記の内容の変更

--- diaries/ShimaPacaDiary.py
+++ diaries/ShimaPacaDiary.py
@@ -3,10 +3,10 @@ from diaries.AbstractDiary import AbstractDiary
 
 class ShimaPacaDiary(AbstractDiary):
     def get_date(self):
-        return "2022-12-08"
+        return "2023-01-01"
 
     def get_summary(self):
-        return "眠たい"
+        return "あけおめ-"
 
     def get_author(self):
         return "ShimaPaca"
